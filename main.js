e9e96ae307577c73989b92942f52849652e13281
simonequarticelli <quarti87@gmail.com>
2019-05-21 22:49:04 +0200

    esercizio ajax-gliglia finito

--- main.js
+++ main.js
@@ -6,5 +6,74 @@
 // Il numero ottenuto appare al centro del quadrato.
 
 $(document).ready(function(){
-  console.log('ciao');
+
+  ///////////////////////////////////////////////////////////////
+
+  //PREPARAZIONE TEMPLATE
+
+  //salvo il template dentro a una variabile
+  var quadrato = $('#template__container').html();
+  //console.log(quadrato);
+
+  //richiamo il compile
+  var template__function = Handlebars.compile(quadrato);
+
+  //genero e appando 36 quadrati
+  for (var i = 0; i < 36; i++) {
+    $('.grid').append(quadrato);
+    //console.log(quadrato);
+  }
+  //console.log(quadrato);
+
+  //////////////////////////////////////////////////////////////
+
+  //al click del quadrato inserisco il numero e la logica
+  $(document).on('click', '#square', function(){
+
+    var square__click = $(this);
+    console.log(square__click);
+
+    //creo la richiesta AJAX
+    $.ajax({
+      'url': "https://www.boolean.careers/api/random/int",
+      'metod': "GET",
+      'success': function(data){ //<-- data <<risposta del server>>
+        
+        var numero = data.response;
+        console.log(numero);
+
+        square__click.text(numero);
+
+        if (numero <= 5) {
+
+          square__click.css({
+            'backgroundColor': '#fff200',
+            'color': 'black',
+          });
+
+        }else if (numero > 5) {
+
+          square__click.css({
+            'backgroundColor': '#27ae60',
+            'color': 'white',
+          });
+        }
+
+      }, 'error': function(richiesta, stato, errori) {
+            alert(errori);
+          }
+
+    });
+
+  });
+
+  ////////////////////////////////////////////////////////////////
+
+
+  // gen_num_ajax(data); //<--estraggo per elaborare tramite la funzione
+
+
+
+
+
 });
