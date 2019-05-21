eed137824969f5673f0f6c02a93beace4206243d
simonequarticelli <quarti87@gmail.com>
2019-05-21 18:01:20 +0200

    inizio esercizio ajax-griglia

---
+++ main.js
@@ -0,0 +1,10 @@
+// Creare una griglia 6x6, ad ogni click su un riquadro parte una richiesta AJAX
+// che prende un numero random da 1 a 9
+// (utilizzare l'API https://www.boolean.careers/api/random/int).
+// Se è <= 5 il quadrato diventa giallo,
+// se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato.
+
+$(document).ready(function(){
+  console.log('ciao');
+});
